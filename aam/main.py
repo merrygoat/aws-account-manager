36f42c3c0475417640db22af1714ddab4d105a63
Peter <13117069+merrygoat@users.noreply.github.com>
2024-11-13 15:16:45 +0000

    Refactor UI into classes

--- aam/main.py
+++ aam/main.py
@@ -1,6 +1,5 @@
 import asyncio
 import datetime
-from functools import partial
 
 import nicegui.events
 from nicegui import ui
@@ -9,7 +8,6 @@ import aam.aws
 from aam import note_dialog
 from aam.models import Account, LastAccountUpdate, Person, Sysadmin, Note
 
-ui_elements = {}
 
 @ui.page('/')
 def main():
@@ -20,200 +18,248 @@ def main():
         Person.create(first_name="Felix", last_name="Edelsten", email="felix@internet.com")
         Person.create(first_name="Connor", last_name="Main", email="connor@internet.com")
 
-    grid_options = {
-        'defaultColDef': {},
-        'columnDefs': [
-            {'headerName': 'Name', 'field': 'name', 'checkboxSelection': True},
-            {'headerName': 'Account ID', 'field': 'id'},
-            {'headerName': 'Root Email', 'field': 'email'},
-            {'headerName': 'Account Status', 'field': 'status'}
-        ]}
-
-    with ui.row().classes('w-full no-wrap'):
-        with ui.column().classes('w-2/3'):
-            ui_elements["grid"]  = ui.aggrid(grid_options)
-        with ui.column().classes('w-1/3'):
-            ui_elements["update_button"] = ui.button("Update Account Info", on_click=update_account_info)
-            ui_elements["last_updated_label"] = ui.label()
-    ui.separator()
-
-    with ui.row().classes('w-full no-wrap'):
-        with ui.column().classes('w-1/2'):
-            ui.html("Account Details").classes("text-2xl")
-            with ui.grid(columns='auto 1fr').classes('w-full'):
-                ui.label("Name:")
-                ui_elements["account_name"] = ui.label("")
-                ui.label("Account ID:")
-                ui_elements["account_id"] = ui.label("")
-                ui.label("Root Email:")
-                ui_elements["root_email"] = ui.label("")
-                ui.label("Account Status:")
-                ui_elements["account_status"] = ui.label("")
-                ui.html("Billing/Contact Details").classes("text-2xl")
-                ui.element()
-                ui.label("Budget Holder:")
-                ui_elements["budget_holder"] = ui.select([], on_change=partial(update_email, "budget_holder")).props("clearable outlined")
-                ui.label("Budget Holder email:")
-                ui_elements["budget_holder_email"] = ui.label("")
-                ui.label("Finance Code:")
-                ui_elements["finance_code"] = ui.input().props("clearable outlined")
-                ui.label("Task Code:")
-                ui_elements["task_code"] = ui.input().props("clearable outlined ")
-                ui.label("Sysadmin:")
-                ui_elements["sysadmin"] = ui.select([], on_change=partial(update_email, "sysadmin")).props("clearable outlined")
-                ui.label("Sysadmin email:")
-                ui_elements["sysadmin_email"] = ui.label("")
-        with ui.column().classes('w-1/2'):
-            ui.html("Notes").classes("text-xl")
-            ui_elements["notes_grid"] = ui.aggrid({
-                'columnDefs': [{"headerName": "id", "field": "id", "hide": True},
-                               {"headerName": "Date", "field": "date"},
-                               {"headerName": "Note", "field": "text"}],
-                'rowData': {},
-                'rowSelection': 'multiple',
-                'stopEditingWhenCellsLoseFocus': True,
-            })
-            note_dialog.add_note_dialog(ui_elements)
-            note_dialog.edit_note_dialog(ui_elements)
-            with ui.row():
-                ui.button('Add note', on_click=lambda: ui_elements["note_dialog"].open())
-                ui.button('Edit note', on_click=partial(note_dialog.open_edit_note_dialog, ui_elements))
-
-
-    ui_elements["save_changes"] = ui.button("Save Changes", on_click=save_changes)
-    ui.button("Freeze", on_click=freeze)
-
-    ui_elements["grid"].on("RowSelected", account_selected)
-    ui_elements["notes_grid"].on("RowDoubleClick", lambda event: ui.notify(event))
-
-
-    ui_elements["budget_holder"].disable()
-    ui_elements["sysadmin"].disable()
-    ui_elements["finance_code"].disable()
-    ui_elements["task_code"].disable()
-    ui_elements["save_changes"].disable()
-    update_last_updated_label()
-    update_account_grid()
+    main_form = MainForm()
 
     ui.run()
 
 
-def freeze():
-    pass
+class MainForm:
+    def __init__(self):
+        with ui.row().classes('w-full no-wrap'):
+            self.account_grid = AccountList(self)
+        ui.separator()
 
-def update_email(email_type: str, event: nicegui.events.ValueChangeEventArguments):
-    selected_sysadmin = event.sender.value
-    if selected_sysadmin:
-        person = Person.get(id=selected_sysadmin)
-        ui_elements[f"{email_type}_email"].set_text(person.email)
-    else:
-        ui_elements[f"{email_type}_email"].set_text("")
+        with ui.row().classes('w-full no-wrap'):
+            with ui.column().classes('w-1/2'):
+                self.account_details = AccountDetails(self)
+            with ui.column().classes('w-1/2'):
+                self.notes = AccountNotes(self)
 
-def account_selected(event: nicegui.events.GenericEventArguments):
-    row_data = event.args['data']
-    if event.args["selected"] is True:
-        ui_elements["sysadmin"].enable()
-        ui_elements["budget_holder"].enable()
-        ui_elements["save_changes"].enable()
-        ui_elements["finance_code"].enable()
-        ui_elements["task_code"].enable()
+        self.save_changes = ui.button("Save Changes", on_click=self.save_account_changes)
+        self.freeze = ui.button("Freeze", on_click=freeze)
 
-        ui_elements["account_name"].set_text(row_data["name"])
-        ui_elements["account_id"].set_text(row_data["id"])
-        ui_elements["root_email"].set_text(row_data["email"])
-        ui_elements["account_status"].set_text(row_data["status"])
-        all_people = {person.id: person.full_name for person in Person.select()}
-        account = Account.get_or_none(Account.id == row_data["id"])
+        self.save_changes.disable()
 
-        ui_elements["budget_holder"].set_options(all_people)
-        ui_elements["finance_code"].set_value(account.finance_code)
-        ui_elements["task_code"].set_value(account.task_code)
-        ui_elements["sysadmin"].set_options(all_people)
-        if account:
-            if account.budget_holder:
-                ui_elements["budget_holder"].set_value(account.budget_holder.id)
-            else:
-                ui_elements["budget_holder"].set_value(None)
-
-            sysadmin = account.sysadmin.get_or_none()
-            if sysadmin:
-                ui_elements["sysadmin"].set_value(sysadmin.person.id)
-            else:
-                ui_elements["sysadmin"].set_value(None)
+    def save_account_changes(self):
+        account = Account.get(Account.id == self.account_details.account_id.text)
+        selected_sysadmin = self.account_details.sysadmin.value
+        if selected_sysadmin:
+            selected_sysadmin = Person.get(Person.id == selected_sysadmin)
+            Sysadmin.create(person=selected_sysadmin, account=account)
         else:
-            ui_elements["sysadmin"].set_value(None)
-            ui_elements["sysadmin"].set_value(None)
+            account.sysadmin.get().delete_instance()
+        selected_budgetholder = self.account_details.budget_holder.value
+        if selected_budgetholder:
+            selected_budgetholder = Person.get(Person.id == selected_budgetholder)
+            account.budget_holder = selected_budgetholder
+        else:
+            account.budget_holder = None
+        ui.notify("Record updated.")
+        account.finance_code = self.account_details.finance_code.value
+        account.task_code = self.account_details.task_code.value
+        account.save()
+
+
+class AccountDetails:
+    def __init__(self, parent: MainForm):
+        self.parent = parent
+
+        ui.html("Account Details").classes("text-2xl")
+        with ui.grid(columns='auto 1fr').classes('w-full'):
+            ui.label("Name:")
+            self.account_name = ui.label("")
+            ui.label("Account ID:")
+            self.account_id = ui.label("")
+            ui.label("Root Email:")
+            self.root_email = ui.label("")
+            ui.label("Account Status:")
+            self.account_status = ui.label("")
+            ui.html("Billing/Contact Details").classes("text-2xl")
+            ui.element()
+            ui.label("Budget Holder:")
+            self.budget_holder = ui.select([], on_change=self.update_budget_holder_email).props(
+                "clearable outlined")
+            ui.label("Budget Holder email:")
+            self.budget_holder_email = ui.label("")
+            ui.label("Finance Code:")
+            self.finance_code = ui.input().props("clearable outlined")
+            ui.label("Task Code:")
+            self.task_code = ui.input().props("clearable outlined ")
+            ui.label("Sysadmin:")
+            self.sysadmin = ui.select([], on_change=self.update_sysadmin_email).props(
+                "clearable outlined")
+            ui.label("Sysadmin email:")
+            self.sysadmin_email = ui.label("")
+
+        self.budget_holder.disable()
+        self.sysadmin.disable()
+        self.finance_code.disable()
+        self.task_code.disable()
+
+    def update_sysadmin_email(self, event: nicegui.events.ValueChangeEventArguments):
+        selected_person = event.sender.value
+        if selected_person:
+            person = Person.get(id=selected_person)
+            self.sysadmin_email.set_text(person.email)
+        else:
+            self.sysadmin_email.set_text("")
+
+    def update_budget_holder_email(self, event: nicegui.events.ValueChangeEventArguments):
+        selected_person = event.sender.value
+        if selected_person:
+            person = Person.get(id=selected_person)
+            self.budget_holder_email.set_text(person.email)
+        else:
+            self.budget_holder_email.set_text("")
+
+    def clear(self):
+        self.account_name.set_text("")
+        self.account_id.set_text("")
+        self.root_email.set_text("")
+        self.account_status.set_text("")
+        self.budget_holder.set_value(None)
+        self.budget_holder_email.set_text("")
+        self.finance_code.set_value(None)
+        self.task_code.set_value(None)
+        self.sysadmin.set_value(None)
+        self.sysadmin_email.set_text("")
+        self.finance_code.disable()
+        self.task_code.disable()
+        self.budget_holder.disable()
+        self.sysadmin.disable()
+
+    def update(self, account_info: dict):
+        self.sysadmin.enable()
+        self.budget_holder.enable()
+        self.finance_code.enable()
+        self.task_code.enable()
+
+        self.account_name.set_text(account_info["account_name"])
+        self.account_id.set_text(account_info["id"])
+        self.root_email.set_text(account_info["email"])
+        self.account_status.set_text(account_info["status"])
+
+        all_people = {person.id: person.full_name for person in Person.select()}
+        self.budget_holder.set_options(all_people)
+        self.sysadmin.set_options(all_people)
+
+        selected_account = account_info["account"]
+        self.finance_code.set_value(selected_account.finance_code)
+        self.task_code.set_value(selected_account.task_code)
+        if selected_account:
+            if selected_account.budget_holder:
+                self.budget_holder.set_value(selected_account.budget_holder.id)
+            else:
+                self.budget_holder.set_value(None)
+
+            sysadmin = selected_account.sysadmin.get_or_none()
+            if sysadmin:
+                self.sysadmin.set_value(sysadmin.person.id)
+            else:
+                self.sysadmin.set_value(None)
+        else:
+            self.sysadmin.set_value(None)
+
+
+class AccountNotes:
+    def __init__(self, parent: MainForm):
+        self.parent = parent
+        ui.html("Notes").classes("text-xl")
+        self.notes_grid = ui.aggrid({
+            'columnDefs': [{"headerName": "id", "field": "id", "hide": True},
+                           {"headerName": "Date", "field": "date"},
+                           {"headerName": "Note", "field": "text"}],
+            'rowData': {},
+            'rowSelection': 'multiple',
+            'stopEditingWhenCellsLoseFocus': True,
+        })
+        self.add_note_dialog = note_dialog.AddNoteDialog(self)
+        self.edit_note_dialog = note_dialog.EditNoteDialog(self)
+        with ui.row():
+            ui.button('Add note', on_click=self.add_note_dialog.open)
+            ui.button('Edit note', on_click=self.edit_note_dialog.open)
+
+    def update_note_grid(self, account: Account):
         notes = [note for note in Note.select().where(Note.account_id == account.id)]
         if notes:
             notes = [{"id": note.id, "date": note.date, "text": note.text} for note in notes]
         else:
             notes = []
-        ui_elements["notes_grid"].options["rowData"] = notes
-        ui_elements["notes_grid"].update()
-
-    elif event.args["selected"] is False and row_data['id'] == ui_elements["account_id"].text:
-        ui_elements["account_name"].set_text("")
-        ui_elements["account_id"].set_text("")
-        ui_elements["root_email"].set_text("")
-        ui_elements["account_status"].set_text("")
-        ui_elements["budget_holder"].set_value(None)
-        ui_elements["budget_holder_email"].set_text("")
-        ui_elements["finance_code"].set_value(None)
-        ui_elements["task_code"].set_value(None)
-        ui_elements["sysadmin"].set_value(None)
-        ui_elements["sysadmin_email"].set_text("")
-        ui_elements["finance_code"].disable()
-        ui_elements["task_code"].disable()
-        ui_elements["budget_holder"].disable()
-        ui_elements["sysadmin"].disable()
-        ui_elements["save_changes"].disable()
+        self.notes_grid.options["rowData"] = notes
+        self.notes_grid.update()
 
 
-def update_last_updated_label():
-    last_account_update = LastAccountUpdate.get_or_none(LastAccountUpdate.id == 0)
-    if last_account_update:
-        ui_elements["last_updated_label"].set_text(
-            f"Account information last updated: {last_account_update.time.strftime('%d/%m/%y, %H:%M:%S')}.")
-    else:
-        ui_elements["last_updated_label"].set_text(f"Account information last updated: None")
+class AccountList:
+    def __init__(self, parent: MainForm):
+        self.parent = parent
 
+        grid_options = {
+            'defaultColDef': {},
+            'columnDefs': [
+                {'headerName': 'Name', 'field': 'name', 'checkboxSelection': True},
+                {'headerName': 'Account ID', 'field': 'id'},
+                {'headerName': 'Root Email', 'field': 'email'},
+                {'headerName': 'Account Status', 'field': 'status'}
+            ]}
 
-def update_account_grid():
-    accounts = [account for account in Account.select()]
-    ui_elements["grid"].options["rowData"] = [account.to_dict() for account in accounts]
-    ui_elements["grid"].update()
+        with ui.column().classes('w-2/3'):
+            self.grid = ui.aggrid(grid_options)
+        with ui.column().classes('w-1/3'):
+            self.update_button = ui.button("Update Account Info", on_click=self.update_account_info)
+            self.last_updated = ui.label()
 
+        self.grid.on("RowSelected", self.account_selected)
+        self.update_last_updated_label()
+        self.update_account_grid()
 
-def save_changes():
-    account = Account.get(Account.id==ui_elements["account_id"].text)
-    selected_sysadmin = ui_elements["sysadmin"].value
-    if selected_sysadmin:
-        selected_sysadmin = Person.get(Person.id == selected_sysadmin)
-        Sysadmin.create(person=selected_sysadmin, account=account)
-    else:
-        account.sysadmin.get().delete_instance()
-    selected_budgetholder = ui_elements["budget_holder"].value
-    if selected_budgetholder:
-        selected_budgetholder = Person.get(Person.id == selected_budgetholder)
-        account.budget_holder = selected_budgetholder
-    else:
-        account.budget_holder = None
-    ui.notify("Record updated.")
-    account.finance_code = ui_elements["finance_code"].value
-    account.task_code = ui_elements["task_code"].value
-    account.save()
+    def account_selected(self, event: nicegui.events.GenericEventArguments):
+        row_data = event.args['data']
+        if event.args["selected"] is True:
+            self.parent.save_changes.enable()
 
-async def update_account_info():
-    with ui.dialog() as loadingDialog:
-        ui.spinner(size='10em', color='black')
-    loadingDialog.open()
+            account = Account.get_or_none(Account.id == row_data["id"])
 
-    await asyncio.to_thread(get_and_process_account_info)
+            account_info = {"account_name": row_data["name"],
+                            "id": row_data["id"],
+                            "email": row_data["email"],
+                            "status": row_data["status"],
+                            "account": account}
 
-    update_last_updated_label()
-    update_account_grid()
+            self.parent.account_details.update(account_info)
 
-    loadingDialog.close()
+            self.parent.notes.update_note_grid(account)
+
+        elif event.args["selected"] is False and row_data['id'] == self.parent.account_details.account_id.text:
+            self.parent.account_details.clear()
+            self.parent.save_changes.disable()
+
+    async def update_account_info(self):
+        with ui.dialog() as loadingDialog:
+            ui.spinner(size='10em', color='black')
+        loadingDialog.open()
+
+        await asyncio.to_thread(get_and_process_account_info)
+
+        self.update_last_updated_label()
+        self.update_account_grid()
+
+        loadingDialog.close()
+
+    def update_last_updated_label(self):
+        last_account_update = LastAccountUpdate.get_or_none(LastAccountUpdate.id == 0)
+        if last_account_update:
+            self.last_updated.set_text(
+                f"Account information last updated: {last_account_update.time.strftime('%d/%m/%y, %H:%M:%S')}.")
+        else:
+            self.last_updated.set_text(f"Account information last updated: None")
+
+    def update_account_grid(self):
+        accounts = [account for account in Account.select()]
+        self.grid.options["rowData"] = [account.to_dict() for account in accounts]
+        self.grid.update()
+
+def freeze():
+    pass
 
 
 def get_and_process_account_info():
--- aam/note_dialog.py
+++ aam/note_dialog.py
@@ -1,79 +1,99 @@
 import functools
 from datetime import datetime
+from typing import TYPE_CHECKING
 
 import nicegui.events
 from nicegui import ui
 
 from aam.models import Note
 
-
-def add_note_dialog(ui_elements: dict):
-    with ui.dialog() as ui_elements["note_dialog"], ui.card():
-        ui.html("Add Note").classes("text-2xl")
-        with ui.input('Date',
-                      value=datetime.today().strftime("%d/%m/%y"),
-                      validation={"Must provide date": lambda value: len(value) > 1}
-                      ) as ui_elements["add_note_date"]:
-            with ui.menu().props('no-parent-event') as menu:
-                with ui.date(mask="DD-MM-YY").bind_value(ui_elements["add_note_date"]):
-                    with ui.row().classes('justify-end'):
-                        ui.button('Close', on_click=menu.close).props('flat')
-            with ui_elements["add_note_date"].add_slot('append'):
-                ui.icon('edit_calendar').on('click', menu.open).classes('cursor-pointer')
-        ui_elements["add_note_text"] = ui.textarea().props('outlined').classes('w-full')
-        with ui.row():
-            ui.button("Save note", on_click=functools.partial(save_new_note, ui_elements))
-            ui.button("Cancel", on_click=lambda: ui_elements["note_dialog"].close())
-
-async def open_edit_note_dialog(ui_elements: dict, event: nicegui.events.ClickEventArguments):
-    note_row = await(ui_elements["notes_grid"].get_selected_row())
-    if note_row:
-        note_id = note_row["id"]
-        note = Note.get(id=note_id)
-        ui_elements["edit_note_id"].text = note.id
-        ui_elements["edit_note_date"].value = note.date
-        ui_elements["edit_note_text"].value = note.text
-        ui_elements["edit_note_dialog"].open()
-    else:
-        ui.notify("No note selected to edit.")
-
-def edit_note_dialog(ui_elements: dict):
-    with ui.dialog() as ui_elements["edit_note_dialog"], ui.card():
-        ui_elements["edit_note_id"] = ui.label("")
-        ui_elements["edit_note_id"].set_visibility(False)
-        ui.html("Edit Note").classes("text-2xl")
-        with ui.input('Date', validation={"Must provide date": lambda value: len(value) > 1}) as ui_elements[
-            "edit_note_date"]:
-            with ui.menu().props('no-parent-event') as menu:
-                with ui.date(mask="DD-MM-YY").bind_value(ui_elements["add_note_date"]):
-                    with ui.row().classes('justify-end'):
-                        ui.button('Close', on_click=menu.close).props('flat')
-            with ui_elements["add_note_date"].add_slot('append'):
-                ui.icon('edit_calendar').on('click', menu.open).classes('cursor-pointer')
-        ui_elements["edit_note_text"] = ui.textarea().props('outlined').classes('w-full')
-        with ui.row():
-            ui.button("Save note", on_click=functools.partial(edit_note, ui_elements))
-            ui.button("Delete note", on_click=functools.partial(delete_note, ui_elements))
-            ui.button("Cancel", on_click=lambda: ui_elements["edit_note_dialog"].close())
-
-def delete_note(ui_elements: dict, event: nicegui.events.ClickEventArguments):
-    ui_elements["note_dialog"].close()
-
-def edit_note(ui_elements: dict, event: nicegui.events.ClickEventArguments):
-    existing_note_id = ui_elements["edit_note_id"].text
-    existing_note = Note.get(id=existing_note_id)
-    existing_note.text = ui_elements["edit_note_text"].value
-    existing_note.date = ui_elements["edit_note_date"].value
-    existing_note.save()
-    ui_elements["edit_note_dialog"].close()
-    ui.notify("Changes to Note saved.")
+if TYPE_CHECKING:
+    from aam.main import AccountNotes
 
 
-async def save_new_note(ui_elements: dict, event: nicegui.events.ClickEventArguments):
-    account_row = await(ui_elements["grid"].get_selected_row())
-    account_id = account_row['id']
-    date = ui_elements["add_note_date"].value
-    text = ui_elements["add_note_text"].value
-    Note.create(date=date, text=text, account_id=account_id)
-    ui_elements["note_dialog"].close()
-    ui.notify("New Note saved.")
+class AddNoteDialog:
+    def __init__(self, parent: "AccountNotes"):
+        self.parent = parent
+
+        with ui.dialog() as self.dialog:
+            with ui.card():
+                self.title = ui.html("Add Note").classes("text-2xl")
+                with ui.input('Date',
+                              value=datetime.today().strftime("%d/%m/%y"),
+                              validation={"Must provide date": lambda value: len(value) > 1}
+                              ) as self.date:
+                    with ui.menu().props('no-parent-event') as menu:
+                        with ui.date(mask="DD-MM-YY").bind_value(self.date):
+                            with ui.row().classes('justify-end'):
+                                ui.button('Close', on_click=menu.close).props('flat')
+                    with self.date.add_slot('append'):
+                        ui.icon('edit_calendar').on('click', menu.open).classes('cursor-pointer')
+                self.text = ui.textarea().props('outlined').classes('w-full')
+                with ui.row():
+                    ui.button("Save note", on_click=functools.partial(self.save_new_note, self.parent))
+                    ui.button("Cancel", on_click=self.close)
+
+    async def save_new_note(self, ui_elements: dict, event: nicegui.events.ClickEventArguments):
+        account_row = await(ui_elements["grid"].get_selected_row())
+        account_id = account_row['id']
+        date = self.date.value
+        text = self.text.value
+        Note.create(date=date, text=text, account_id=account_id)
+        self.close()
+        ui.notify("New Note saved.")
+
+    def open(self):
+        self.dialog.open()
+
+    def close(self):
+        self.dialog.close()
+
+
+class EditNoteDialog:
+    def __init__(self, parent: "AccountNotes"):
+        self.parent = parent
+        self.note_id = ""
+
+        with ui.dialog() as self.dialog:
+            with ui.card():
+                self.title = ui.html("Edit Note").classes("text-2xl")
+                with ui.input('Date', validation={"Must provide date": lambda value: len(value) > 1}) as self.date_input:
+                    with ui.menu().props('no-parent-event') as menu:
+                        with ui.date(mask="DD-MM-YY").bind_value(self.date_input):
+                            with ui.row().classes('justify-end'):
+                                ui.button('Close', on_click=menu.close).props('flat')
+                    with self.date_input.add_slot('append'):
+                        ui.icon('edit_calendar').on('click', menu.open).classes('cursor-pointer')
+                self.text = ui.textarea().props('outlined').classes('w-full')
+                with ui.row():
+                    ui.button("Save note", on_click=self.save_edit)
+                    ui.button("Delete note", on_click=self.delete_note)
+                    ui.button("Cancel", on_click=self.close)
+
+    def close(self):
+        self.dialog.close()
+
+    async def open(self, event: nicegui.events.ClickEventArguments):
+        note_row = await(self.parent.notes_grid.get_selected_row())
+        if note_row:
+            note_id = note_row["id"]
+            note = Note.get(id=note_id)
+            self.note_id = note.id
+            self.date_input.value = note.date
+            self.text.value = note.text
+            self.dialog.open()
+        else:
+            ui.notify("No note selected to edit.")
+
+    def delete_note(self, event: nicegui.events.ClickEventArguments):
+        self.close()
+
+    def save_edit(self, event: nicegui.events.ClickEventArguments):
+        existing_note_id = self.note_id
+        existing_note = Note.get(id=existing_note_id)
+        existing_note.text = self.text.value
+        existing_note.date = self.date_input.value
+        existing_note.save()
+        self.close()
+        ui.notify("Changes to Note saved.")
+
