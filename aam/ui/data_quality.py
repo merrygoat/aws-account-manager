6338ba889ebb63f83a6337c81e6a6df6e60e6b9a
Peter <13117069+merrygoat@users.noreply.github.com>
2025-02-25 14:24:33 +0000

    Add data quality element to find MonthlyUsage when account is not open

--- aam/ui/data_quality.py
+++ aam/ui/data_quality.py
@@ -1,6 +1,7 @@
 import datetime
 from typing import TYPE_CHECKING, Iterable
 
+import nicegui.events
 from nicegui import ui
 
 from aam.models import Account, Organization, MonthlyUsage
@@ -43,7 +44,7 @@ class UIDataQuality:
 
             with ui.tab_panel(tab_two):
                 with ui.column().classes('w-full no-wrap'):
-                    ui.html("Accounts Missing Monthly Usage").classes("text-2xl")
+                    ui.label("Accounts Missing Monthly Usage").classes("text-2xl")
                     self.missing_usage = ui.aggrid({
                         'defaultColDef': {"suppressMovable": True},
                         'columnDefs': [{"headerName": "Account", "field": "account_name"},
@@ -56,9 +57,24 @@ class UIDataQuality:
                         'rowData': {}})
                     ui.button("Refresh list", on_click=self.populate_missing_usage_grid)
 
+                    ui.label("Accounts with Monthly Usage outside of open/close dates").classes("text-2xl")
+                    self.wrong_monthly_usage = ui.aggrid({
+                        'defaultColDef': {"suppressMovable": True},
+                        'columnDefs': [{"headerName": "MonthlyUsageID", "field": "id"},
+                                       {"headerName": "Account", "field": "account_name"},
+                                       {"headerName": "Account ID", "field": "account_id", "sort": "asc"},
+                                       {"headerName": "Date", "field": "date"},
+                                       {"headerName": "Usage", "field": "usage"}
+                                       ],
+                        'rowSelection': 'multiple',
+                        'rowData': {}})
+                    with ui.row():
+                        ui.button("Refresh list", on_click=self.populate_wrong_monthly_usage)
+                        ui.button("Delete selected months", on_click=self.delete_selected_monthly_usage)
+
+
             self.populate_no_open_grid()
             self.populate_no_close_grid()
-
 
     def populate_no_open_grid(self):
         accounts = Account.select().join(Organization).where(Account.creation_date.is_null(True))
@@ -100,3 +116,34 @@ class UIDataQuality:
 
         self.missing_usage.options["rowData"] = account_summaries
         self.missing_usage.update()
+
+    def populate_wrong_monthly_usage(self):
+        """Remove any MonthlyUsage transactions which fall before the opening date or after the closing date of an
+        account."""
+        all_usage: Iterable[MonthlyUsage] = MonthlyUsage.select(MonthlyUsage, Account).join(Account)
+        usage_details = []
+        for usage in all_usage:
+            account: Account = usage.account
+            if not account.creation_date:
+                pass
+            start_month = aam.utilities.month_code(account.creation_date.year, account.creation_date.month)
+            if account.closure_date:
+                end_month = aam.utilities.month_code(account.closure_date.year, account.closure_date.month)
+            else:
+                end_month = aam.utilities.month_code(datetime.date.today().year, datetime.date.today().month)
+            if usage.month_id < start_month or usage.month_id > end_month:
+                usage_details.append({"id": usage.id, "account_name": usage.account.name,
+                                      "account_id": usage.account.id, "date": usage.date, "usage": usage.amount})
+        self.wrong_monthly_usage.options["rowData"] = usage_details
+        self.wrong_monthly_usage.update()
+
+    async def delete_selected_monthly_usage(self, event: nicegui.events.ClickEventArguments):
+        selected_rows = await(self.wrong_monthly_usage.get_selected_rows())
+        if not selected_rows:
+            ui.notify("No rows selected to delete.")
+            return 0
+        monthly_usage_ids = [row["id"] for row in selected_rows]
+        query = MonthlyUsage.delete().where(MonthlyUsage.id.in_(monthly_usage_ids))
+        rows_deleted = query.execute()
+        self.populate_wrong_monthly_usage()
+        ui.notify(f"{rows_deleted} rows deleted")
